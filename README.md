65b007b14482745534062e405517c3bb347e9758
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-07-05 17:25:28 -0400

    generate 0.1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# generate
+[![Build Status](https://travis-ci.org/sweet-tooth-clojure/generate.svg?branch=master)](https://travis-ci.org/sweet-tooth-clojure/generate)
+[![codecov](https://codecov.io/gh/sweet-tooth-clojure/generate/branch/master/graph/badge.svg)](https://codecov.io/gh/sweet-tooth-clojure/generate)
+[![Clojars Project](https://img.shields.io/clojars/v/sweet-tooth/generate.svg)](https://clojars.org/sweet-tooth/generate)
+
+A Clojure library designed to ... well, that part is up to you.
+
+```clj
+[sweet-tooth/generate "0.0.0"]
+```
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2018 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject sweet-tooth/generate "0.1.0"
+  :description "Generate code from the REPL"
+  :url "https://github.com/sweet-tooth-clojure/generate"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[cljstache "2.0.6"]
+                 [rewrite-clj "0.6.1"]]
+  :plugins [[lein-cloverage "1.0.13"]
+            [lein-shell "0.5.0"]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.3.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :deploy-repositories [["releases" :clojars]]
+  :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[sweet-tooth\\\\/generate \"[0-9.]*\"\\\\]/[sweet-tooth\\\\/generate \"${:version}\"]/" "README.md"]}
+  :release-tasks [["shell" "git" "diff" "--exit-code"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["changelog" "release"]
+                  ["update-readme-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["vcs" "push"]])
